9cb3ea74b5f08a983e61a9fde0e3f9fd22fea399
Nicholas M. Synovic <Nicholas.Synovic@GMail.com>
2021-02-13 17:21:16 -0600

    Create chessboard

---
+++ chess.js
@@ -0,0 +1,44 @@
+const chessBoard = document.getElementById("chessBoard")
+const chessBoardRows = document.getElementsByClassName("chessBoardRow")
+const files = ["a", "b", "c", "d", "e", "f", "g", "h"]
+
+
+function makeBoard() {
+    for (rowCount = 0; rowCount < 8; rowCount++) {
+        var chessBoardRow = document.createElement("tr")
+        chessBoardRow.className = "chessBoardRow"
+        chessBoard.append(chessBoardRow)
+    }
+
+    for (index = 0; index < chessBoardRows.length; index++) {
+        if (index % 2 == 0) {
+            _chessBoardSquareAdder(chessBoardRows[index], "blackSquare", "whiteSquare", index)
+        }
+        else {
+            _chessBoardSquareAdder(chessBoardRows[index], "whiteSquare", "blackSquare", index)
+        }
+    }
+}
+
+/**
+ *
+ * @param {Node} rankNode
+ * @param {String} startingColorID
+ * @param {String} nextColorID
+ * @param {Number} rank
+ */
+function _chessBoardSquareAdder(rankNode, startingColorID, nextColorID, rank) {
+    for (pieceCount = 0; pieceCount < 8; pieceCount++) {
+        var piece = document.createElement("td")
+        if (pieceCount % 2 == 0) {
+            piece.id = startingColorID
+        }
+        else {
+            piece.id = nextColorID
+        }
+        piece.className = files[pieceCount] + rank
+        rankNode.appendChild(piece)
+    }
+}
+
+makeBoard()

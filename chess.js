4b8ee04b5bb4e9bdbe87f493c3ae514c230aec47
Nicholas M. Synovic <Nicholas.Synovic@GMail.com>
2021-02-14 15:08:41 -0600

    Show a potential first move for a pawn

--- chess.js
+++ chess.js
@@ -2,7 +2,19 @@ const chessBoard = document.getElementById("chessBoard")
 const chessBoardRows = document.getElementsByClassName("chessBoardRow")
 const files = ["a", "b", "c", "d", "e", "f", "g", "h"]
 
-let boardState = null
+const blackRook = "♜"
+const blackKnight = "♞"
+const blackBishop = "♝"
+const blackQueen = "♛"
+const blackKing = "♚"
+const blackPawn = "♟︎"
+const whitePawn = "♙"
+const whiteRook = "♖"
+const whiteKnight = "♘"
+const whiteBishop = "♗"
+const whiteQueen = "♕"
+const whiteKing = "♔"
+
 
 function getFIDEInitialBoardState() {
     var request = new XMLHttpRequest()
@@ -44,6 +56,40 @@ function placePieces(boardState) {
     }
 }
 
+function removePotentialMoves(boardState) {
+    boardState = boardState[0]
+    for (file in boardState) {
+        for (rank in boardState[file])
+            if (boardState[file][rank]["currentPiece"] == "○") {
+                boardState[file][rank]["currentPiece"] = ""
+            }
+    }
+}
+
+/**
+ * Displays the availible moves a pawn can make on the board
+ * @param {JSON} boardState
+ * @param {String} pawn
+ * @param {String} coordinates
+ */
+function showMoves_Pawn(boardState, pawn, coordinates) {
+    const splitCoordinates = coordinates.split("")
+    const originalRank = splitCoordinates[1]
+    const originalFile = splitCoordinates[0]
+    if (pawn == whitePawn) {
+        // Move up (6 to 0)
+        if (boardState[0][originalFile][originalRank - 1]["currentPiece"] == "") {
+            boardState[0][originalFile][originalRank - 1]["currentPiece"] = "○"
+            placePieces(boardState)
+        }
+        // Take diagnol
+    }
+    else {
+        // Move down (1 to 7)
+        // Take diagnol
+        return NaN
+    }
+}
 
 /**
  * Appends the board sqaures to the DOM
@@ -55,6 +101,8 @@ function placePieces(boardState) {
 function _chessBoardSquareAdder(rankNode, startingColorID, nextColorID, rank) {
     for (pieceCount = 0; pieceCount < 8; pieceCount++) {
         var piece = document.createElement("td")
+        piece.onmouseover = function () { showMoves_Pawn(boardState, this.innerHTML, this.className) } // This appends the selector method
+        piece.onmouseleave = function () { removePotentialMoves(boardState) }
         if (pieceCount % 2 == 0) {
             piece.id = startingColorID
         }
@@ -67,5 +115,5 @@ function _chessBoardSquareAdder(rankNode, startingColorID, nextColorID, rank) {
 }
 
 makeBoard()
-boardState = getFIDEInitialBoardState()
+let boardState = getFIDEInitialBoardState()
 placePieces(boardState)
